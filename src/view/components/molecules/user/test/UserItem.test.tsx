f0a02e43316d0d437d3c23a3dbe91c11bb920781
sodavinchheng-quad <sodavin.chheng@quad.co.jp>
2023-09-06 19:39:17 +0900

    feat: add mock data

--- src/view/components/molecules/user/test/UserItem.test.tsx
+++ src/view/components/molecules/user/test/UserItem.test.tsx
@@ -2,29 +2,9 @@ import { render, screen } from "@testing-library/react";
 import { UserItem } from "../UserItem";
 import { User } from "../../../../../domain/model";
 
-const mockUser: User = {
-  id: 1,
-  name: "User One",
-  username: "username",
-  email: "userone@example.com",
-  tel: "+9112800800",
-  website: "www.example.com",
-  company: {
-    bs: "bs",
-    catchPhrase: "Catch Phrase",
-    name: "My Company",
-  },
-  address: {
-    suite: "123",
-    street: "Mango Avenue",
-    city: "New York",
-    zipcode: "1230001",
-    geo: {
-      lat: "27.0021",
-      lng: "138.2902",
-    },
-  },
-};
+import users from "../../../../../mock/users.json";
+
+const mockUser: User = users[0];
 
 test("Text component renders the text based on prop input", async () => {
   render(<UserItem user={mockUser} />);
@@ -33,5 +13,5 @@ test("Text component renders the text based on prop input", async () => {
   expect(screen.getByText("User One")).toBeInTheDocument();
   expect(screen.getByText("userone@example.com")).toBeInTheDocument();
   expect(screen.getByText("+9112800800")).toBeInTheDocument();
-  expect(screen.getByText("My Company")).toBeInTheDocument();
+  expect(screen.getByText("My Company 1")).toBeInTheDocument();
 });
